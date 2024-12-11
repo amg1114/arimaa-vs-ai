2be793b0b9f883112345660f11b18af81d2dc093
Alejandro Moreno <alejandromogil@gmail.com>
2024-12-11 15:40:58 -0500

    feat: add global game instance and display tile indices on the board

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
-import './style.css'
+import "./style.css";
 
-import { Game } from './class/Game.ts'
-import { Piece } from './class/pieces/Piece.ts'
+declare global {
+    interface Window {
+        game: Game;
+    }
+}
 
-// document ready 
+import { Game } from "./class/Game.ts";
+import { Piece } from "./class/pieces/Piece.ts";
+
+// document ready
 var game = new Game();
 
-const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
-const ctx = canvas.getContext('2d')!;
+window.game = game;
+
+const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
+const ctx = canvas.getContext("2d")!;
 
 // Ajustar el tamaño del canvas para alta resolución
 canvas.width = 600;
@@ -17,42 +25,47 @@ canvas.height = 600;
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
-
 game.fillBoard();
 
 function gameLoop() {
-  drawBoard();
+    drawBoard();
 
-  requestAnimationFrame(gameLoop);
+    requestAnimationFrame(gameLoop);
 }
 
 function drawBoard() {
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-  for (let i = 0; i < game.board.length; i++) {
-    for (let j = 0; j < game.board[i].length; j++) {
-      const height = canvasHeight / game.board.length;
-      const width = canvasWidth / game.board[i].length;
+    for (let i = 0; i < game.board.length; i++) {
+        for (let j = 0; j < game.board[i].length; j++) {
+            const height = canvasHeight / game.board.length;
+            const width = canvasWidth / game.board[i].length;
+            
+           // add index to tile
+           ctx.font = "10px Arial";
+           ctx.fillStyle = "black";
+           ctx.fillText(`${i}, ${j}`, j * width + 5, i * height + 10);
 
-      if (game.board[i][j] instanceof Piece) {
-        const piece = game.board[i][j] as Piece;
-        const image = new Image();
+            if (game.board[i][j] instanceof Piece) {
+                const piece = game.board[i][j] as Piece;
+                const image = new Image();
 
-        image.src = `/pieces/${ piece.color.toLowerCase() }/${ piece.name.toLocaleLowerCase() }.svg`;
-        // console.log("add img",image.src);
-        ctx.drawImage(image, j * width, i * height, width, height);
-      }else {
-        const tile = game.board[i][j] as number;
+                image.src = `/pieces/${piece.color.toLowerCase()}/${piece.name.toLocaleLowerCase()}.svg`;
 
-        if (tile === 1) {
-          ctx.fillStyle = 'gray';
-          ctx.fillRect(j * width, i * height, width, height);
+
+                ctx.drawImage(image, j * width, i * height, width, height);
+            } else {
+                const tile = game.board[i][j] as number;
+
+                if (tile === 1) {
+                    ctx.fillStyle = "gray";
+                    ctx.fillRect(j * width, i * height, width, height);
+                }
+            }
+
+           
         }
-
-      }
     }
-  }
-
 }
 
 gameLoop();
