f667bf6570595e177d97140668e6f9bf19a0317a
Alejandro Moreno <alejandromogil@gmail.com>
2024-12-11 13:25:24 -0500

    feat: enhance canvas layout and add new animal pieces with SVG images

--- src/class/Game.ts
+++ src/class/Game.ts
@@ -1,3 +1,8 @@
+import { Camel } from "./pieces/Camel";
+import { Cat } from "./pieces/Cat";
+import { Dog } from "./pieces/Dog";
+import { Elephant } from "./pieces/Elephant";
+import { Horse } from "./pieces/Horse";
 import { Piece } from "./pieces/Piece";
 import { Rabbit } from "./pieces/Rabbit";
 
@@ -27,8 +32,30 @@ export class Game {
                     this.board[i][j] = new Rabbit(i === 0 ? "silver" : "gold", [i, j]);
                 }
 
-            
+                // add horses
+                if ((j === 0 || j === this.board[i].length - 1) && (i === 1 || i === this.board.length - 2)) {
+                    this.board[i][j] = new Horse(i === 1 ? "silver" : "gold", [i, j]);
+                }
 
+                // add dogs
+                if ((j === 1 || j === this.board[i].length - 2) && (i === 1 || i === this.board.length - 2)) {
+                    this.board[i][j] = new Dog(i === 1 ? "silver" : "gold", [i, j]);
+                }
+
+                // add cats
+                if ((j === 2 || j === this.board[i].length - 3) && (i === 1 || i === this.board.length - 2)) {
+                    this.board[i][j] = new Cat(i === 1 ? "silver" : "gold", [i, j]);
+                }
+
+                // add camel
+                if (j === 3 && (i === 1 || i === this.board.length - 2)) {
+                    this.board[i][j] = new Camel(i === 1 ? "silver" : "gold", [i, j]);
+                }
+
+                // add elephant
+                if (j === 4 && (i === 1 || i === this.board.length - 2)) {
+                    this.board[i][j] = new Elephant(i === 1 ? "silver" : "gold", [i, j]);
+                }
             }
         }
     }
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,15 @@ var game = new Game();
 const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
 const ctx = canvas.getContext('2d')!;
 
+// Ajustar el tamaño del canvas para alta resolución
+canvas.width = 600;
+canvas.height = 600;
+// ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
+
 game.fillBoard();
 
 function gameLoop() {
@@ -18,17 +27,28 @@ function gameLoop() {
 }
 
 function drawBoard() {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
   for (let i = 0; i < game.board.length; i++) {
     for (let j = 0; j < game.board[i].length; j++) {
-      const height = canvas.height / game.board.length;
-      const width = canvas.width / game.board[i].length;
+      const height = canvasHeight / game.board.length;
+      const width = canvasWidth / game.board[i].length;
 
       if (game.board[i][j] instanceof Piece) {
         const piece = game.board[i][j] as Piece;
-        ctx.fillStyle = piece.color;
-        
+        const image = new Image();
 
-        ctx.fillRect(piece.position[1] * width, piece.position[0] * height, width, height);
+        image.src = `/pieces/${ piece.color.toLowerCase() }/${ piece.name.toLocaleLowerCase() }.svg`;
+        // console.log("add img",image.src);
+        ctx.drawImage(image, j * width, i * height, width, height);
+      }else {
+        const tile = game.board[i][j] as number;
+
+        if (tile === 1) {
+          ctx.fillStyle = 'gray';
+          ctx.fillRect(j * width, i * height, width, height);
+        }
+
       }
     }
   }
