b381369c07a9ab072c6060d20464acdfd6d57656
Alejandro Moreno <alejandromogil@gmail.com>
2024-12-11 11:26:23 -0500

    feat: add Horse piece class and update weights constants

---
+++ src/class/pieces/Horse.tsx
@@ -0,0 +1,13 @@
+import { HORSE_WEIGHT } from "../../constants/weights.constant";
+import { ColorPiece } from "../../types/color-piece.type";
+import { Piece } from "./Piece";
+
+export class Horse extends Piece {
+    constructor(color: ColorPiece, position: number[]) {
+        const weight = HORSE_WEIGHT;
+        super(color, weight, position);
+    }
+    move() {
+        console.log("Horse move");
+    }
+}
--- src/constants/weights.constant.ts
+++ src/constants/weights.constant.ts
@@ -4,21 +4,26 @@
 export const RABBIT_WEIGHT = 1;
 
 /**
- * The game weight of a rabbit.
+ * The game weight of a cat.
  */
 export const CAT_WEIGHT = 2;
 
 /**
- * The game weight of a rabbit.
+ * The game weight of a dog.
  */
 export const DOG_WEIGHT = 3;
 
 /**
- * The game weight of a rabbit.
+ * The game weight of a horse.
  */
-export const CAMEL_WEIGHT = 4;
+export const HORSE_WEIGHT = 4;
 
 /**
- * The game weight of a rabbit.
+ * The game weight of a camel.
  */
-export const ELEPHANT_WEIGHT = 5;
+export const CAMEL_WEIGHT = 5;
+
+/**
+ * The game weight of a elephant.
+ */
+export const ELEPHANT_WEIGHT = 6;
